de7405e2b5a1ae2fddc884042df452ab39b42d93
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-21 11:02:28 +0800

    第二章 netty入门 2.helloServer-Client流程交互

--- src/test/java/cn/itcast/netty/c1/HelloClient.java
+++ src/test/java/cn/itcast/netty/c1/HelloClient.java
@@ -15,23 +15,28 @@ import java.util.Date;
  */
 public class HelloClient {
     public static void main(String[] args) throws InterruptedException {
-        //1.启动类
+        //1.启动类   7 第7步
         new Bootstrap()
-                //2。添加EventLoop
+                //2。添加EventLoop  8 第八步
                 .group(new NioEventLoopGroup())
-                //3.选择客户端Channel
+                //3.选择客户端Channel   9 第9步
                 .channel(NioSocketChannel.class)
-                //4.添加处理器
+                //4.添加处理器           10 第10步
                 .handler(new ChannelInitializer<NioSocketChannel>() {
-                    @Override // 链接建立后调用
+                    @Override // 链接建立后调用  //12 第12步 链接建立后 调用初始化方法（客户端执行）
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ch.pipeline().addLast(new StringEncoder());
+                                                // 15.把hello转成byteBuf
                     }
                 })
-                // 5.链接到服务器
+                // 5.链接到服务器     11 第11步
                 .connect(new InetSocketAddress(8080))
+                // 13.第13步 阻塞方法直到链接建立
                 .sync()
+                // 客户端和服务器之间的SocketChannel 链接对象
                 .channel()
-                .writeAndFlush(new Date() + ": hello world!"); // 7
+                .writeAndFlush(new Date() + ": hello world!"); // 14. 第14步 发送数据 会走到处理器类 StringEncoder()里面
+                                                                    //注意 不是走到28行的addlast()那边，而是StringEncoder
+                                                                    // 是个Handler，我们走到Handler里面
     }
 }
--- src/test/java/cn/itcast/netty/c1/HelloServer.java
+++ src/test/java/cn/itcast/netty/c1/HelloServer.java
@@ -18,28 +18,32 @@ public class HelloServer {
         //1.服务器端启动器 功能是负责组装netty组件 ，启动服务器。
         new ServerBootstrap()
                 // 2.bossEventLoop workerEventLoop（可以翻译为循环处理事件）。可以简单理解为 `线程池 + Selector`
+                // 16 也是第16步 将hello转为bytebuf之后，由某个EventLoop处理read事件，接收到ByteBuf
                 .group(new NioEventLoopGroup())
                 // 3.选择服务器的serverSocketChannel实现
                 .channel(NioServerSocketChannel.class)
                 // 4.这边的child和worker是一个意思。boss是处理链接的，worker是处理读写的。
                 // child就是我们的worker（child）是要执行哪些操作handler
                 .childHandler(
-                        //5.Channel代表客户端进行数据读写通道 initializer的初始化。负责添加别的Handler
+                        //5.添加初始化器，只有链接建立后才会执行。
+                        // Channel代表客户端进行数据读写通道 initializer的初始化。负责添加别的Handler
                         //为啥方法叫 childHandler，是接下来添加的处理器都是给 SocketChannel 用的，而不是给 ServerSocketChannel。
                         // ChannelInitializer 处理器（仅执行一次），它的作用是待客户端 SocketChannel 建立连接后，执行 initChannel 以便添加更多的处理器
                         new ChannelInitializer<NioSocketChannel>() {
-                    @Override
+                    @Override //12 第12步 链接建立后 调用初始化方法（服务端执行）
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        //6.添加具体的Handler
-                        ch.pipeline().addLast(new StringDecoder());//将byteBuf转化为字符串
+                        //添加具体的Handler
+                        ch.pipeline().addLast(new StringDecoder());//17 第17步 将byteBuf转化为字符串。还原hello
                         ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){//自定义的Handler
                             @Override //读事件发生之后
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                //打印上一步转化的字符串
+                                //打印上一步转化的字符串   18.执行read方法，打印由17步还原出来的hello
                                 System.out.println(msg);;
                             }
                         });
                     }
-                }).bind(8080);
+                })
+                //6.绑定端口
+                .bind(8080);
     }
 }
