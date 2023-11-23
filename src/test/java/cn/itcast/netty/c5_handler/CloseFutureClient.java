2b1a8815ac3008bb84f0f0670b46cd0a3d587b65
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-23 11:38:43 +0800

    netty pipeline 1.入站 按照顺序进行处理 2.出站 是倒着来的，从tail往head运行 3.出站 一定必须要触发channel的write操作才行

---
+++ src/test/java/cn/itcast/netty/c5_handler/CloseFutureClient.java
@@ -0,0 +1,68 @@
+package cn.itcast.netty.c5_handler;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Scanner;
+
+/**
+ * @author XuHan
+ * @date 2023/11/22 15:52
+ */
+@Slf4j
+public class CloseFutureClient {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        ChannelFuture channelFuture = new Bootstrap()
+                .group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                //1.链接服务器
+                // connect是一个异步非阻塞的 main线程发起了调用 真正执行连接的的是NIO线程
+                .connect("127.0.0.1", 8080);//1s后 建立连接
+
+        Channel channel = channelFuture.sync().channel();
+        new Thread(()->{
+            Scanner scanner = new Scanner(System.in);
+            while (true){
+                String line = scanner.nextLine();
+                if("q".equals(line)){
+                    channel.close();//close方法也是一个异步操作 可能是1s之后才真正关闭
+                    break;
+                }
+                channel.writeAndFlush(line);
+            }
+        },"input").start();
+
+        // 获取一个新的future对象-closeFuture对象 1）同步处理结果 2）异步处理结果
+        // ChannelFuture closeFuture = channel.closeFuture();
+        // System.out.println("waiting close");
+        // closeFuture.sync();
+        // log.debug("处理关闭结果");
+
+        //方法2
+        ChannelFuture closeFuture = channel.closeFuture();
+        closeFuture.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                log.debug("NIO线程处理关闭结果");
+                group.shutdownGracefully();
+            }
+        });
+    }
+}
---
+++ src/test/java/cn/itcast/netty/c5_handler/PipelineServer.java
@@ -0,0 +1,74 @@
+package cn.itcast.netty.c5_handler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author XuHan
+ * @date 2023/11/23 10:53
+ */
+@Slf4j
+public class PipelineServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        // 1.通过Channel拿到pipeline
+                        ChannelPipeline pipeline = ch.pipeline();
+                        // 2.添加处理器 head<->h1<->h2<->h3<->tail
+                        // inBound
+                        pipeline.addLast("h1",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("1");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        pipeline.addLast("h2",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("2");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        pipeline.addLast("h3",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("3");
+                                super.channelRead(ctx, msg);
+                                ch.writeAndFlush(ctx.alloc().buffer().writeBytes("server..".getBytes()));
+                            }
+                        });
+                        // outBound 只有向channel中写了数据才会触发
+                        pipeline.addLast("h4",new ChannelOutboundHandlerAdapter(){
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("4");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("h5",new ChannelOutboundHandlerAdapter(){
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("5");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("h6",new ChannelOutboundHandlerAdapter(){
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("6");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                    }
+                }).bind(8080);
+    }
+}
