e703f339146d80670518e4ed1b4ede01f47680a6
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-27 14:45:37 +0800

    粘包和半包 粘包现象，我想让服务器分10次接受我的数据，没批16个。 结果它一股脑全部整了。

---
+++ src/test/java/cn/itcast/netty_advance/c01_stickPackage/HelloWorldClient.java
@@ -0,0 +1,50 @@
+package cn.itcast.netty_advance.c01_stickPackage;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author XuHan
+ * @date 2023/11/27 14:34
+ */
+@Slf4j
+public class HelloWorldClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try{
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(worker);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) {
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                        //会在链接建立成功后 就会触发这个事件
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            for (int i = 0; i < 10; i++) {
+                                ByteBuf buf = ctx.alloc().buffer(16);
+                                buf.writeBytes(new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15});
+                                ctx.writeAndFlush(buf);
+                            }
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        }catch (Exception e){
+            log.error("client error", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
---
+++ src/test/java/cn/itcast/netty_advance/c01_stickPackage/HelloWorldServer.java
@@ -0,0 +1,50 @@
+package cn.itcast.netty_advance.c01_stickPackage;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author XuHan
+ * @date 2023/11/27 14:28
+ */
+@Slf4j
+public class HelloWorldServer {
+
+    void start() {
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080);
+            log.debug("{} binding...", channelFuture.channel());
+            channelFuture.sync();
+            log.debug("{} bound...", channelFuture.channel());
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+            log.debug("stoped");
+        }
+    }
+
+    public static void main(String[] args) {
+        new HelloWorldServer().start();
+    }
+}
