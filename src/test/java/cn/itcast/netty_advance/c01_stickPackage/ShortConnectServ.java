215264e8b73f911443cc0b748fc1457b65618a6a
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-27 15:43:46 +0800

    粘包和半包解决方案： 1.粘包解决：短连接。 发一个包建立一次连接，这样连接建立到连接断开之间就是消息的边界，缺点效率太低 而且还是会存在半包问题。

---
+++ src/test/java/cn/itcast/netty_advance/c01_stickPackage/ShortConnectServ.java
@@ -0,0 +1,55 @@
+package cn.itcast.netty_advance.c01_stickPackage;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.AdaptiveRecvByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author XuHan
+ * @date 2023/11/27 15:25
+ */
+@Slf4j
+public class ShortConnectServ {
+    void start() {
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            // 调整系统的接受缓冲器（滑动窗口）
+            // serverBootstrap.option(ChannelOption.SO_RCVBUF,7);
+            // 调整netty 的接受缓冲区（byteBuf）
+            serverBootstrap.childOption(ChannelOption.RCVBUF_ALLOCATOR,new AdaptiveRecvByteBufAllocator(10,10,10));
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080);
+            log.debug("{} binding...", channelFuture.channel());
+            channelFuture.sync();
+            log.debug("{} bound...", channelFuture.channel());
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+            log.debug("stoped");
+        }
+    }
+
+    public static void main(String[] args) {
+        new ShortConnectServ().start();
+    }
+}
