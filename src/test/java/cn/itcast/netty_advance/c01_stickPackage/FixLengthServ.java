06db047c7c64085fbc57faee2810fc30e7741734
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-27 16:05:54 +0800

    粘包和半包解决方案： 让所有数据包长度固定（假设长度为 8 字节），服务器端加入， ch.pipeline().addLast(new FixedLengthFrameDecoder(8)); 虽然可以解决粘包半包问题，但是会带来资源的浪费

---
+++ src/test/java/cn/itcast/netty_advance/c01_stickPackage/FixLengthServ.java
@@ -0,0 +1,55 @@
+package cn.itcast.netty_advance.c01_stickPackage;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author XuHan
+ * @date 2023/11/27 15:53
+ */
+@Slf4j
+public class FixLengthServ {
+    public void start() {
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            // 设置接受缓冲区
+            serverBootstrap.option(ChannelOption.SO_RCVBUF,7);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new FixedLengthFrameDecoder(8));
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080);
+            log.debug("{} binding...", channelFuture.channel());
+            channelFuture.sync();
+            log.debug("{} bound...", channelFuture.channel());
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+            log.debug("stoped");
+        }
+    }
+
+    public static void main(String[] args) {
+        new FixLengthServ().start();
+    }
+
+}
