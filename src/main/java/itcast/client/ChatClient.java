5bba8d6d7cba0186d7cb5206dc538d93657c8533
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-29 10:48:12 +0800

    聊天业务： 登录

--- src/main/java/itcast/client/ChatClient.java
+++ src/main/java/itcast/client/ChatClient.java
@@ -2,15 +2,21 @@ package itcast.client;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import itcast.message.LoginRequestMessage;
 import itcast.protocol.MessageCodecShareable;
 import itcast.protocol.ProtocolFrameDecoder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author XuHan
@@ -28,11 +34,45 @@ public class ChatClient {
             bootstrap.channel(NioSocketChannel.class);
             bootstrap.group(group);
             bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                // 接收服务器给我的输入
+                @Override
+                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                    log.debug("message：{}",msg);
+                }
+
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
                     ch.pipeline().addLast(loggingHandler);
                     ch.pipeline().addLast(messageCodec);
+                    ch.pipeline().addLast("clientHandler",new ChannelInboundHandlerAdapter(){
+                        // 在链接建立后会触发这个事件
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            // 用户输入毕竟是阻塞IO，需要一个独立线程 负责用户在和控制台的输入，向服务器发送消息
+                            new Thread(()->{
+                                Scanner scanner=new Scanner(System.in);
+                                System.out.println("请输入用户名：");
+                                String name = scanner.nextLine();
+                                System.out.println("请输入密码：");
+                                String password = scanner.nextLine();
+                                // 构造消息对象
+                                LoginRequestMessage loginRequestMessage = new LoginRequestMessage(name, password);
+                                // 发送消息 （通过ctx发消息出去，就是从tail往前找handler）
+                                ctx.writeAndFlush(loginRequestMessage);
+
+                                System.out.println("等待后续操作");
+                                try {
+                                    int read = System.in.read();
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            },"system in").start();
+                        }
+                    });
+
+
+
                 }
             });
             Channel channel = bootstrap.connect("localhost", 8080).sync().channel();
--- src/main/java/itcast/server/ChatServer.java
+++ src/main/java/itcast/server/ChatServer.java
@@ -2,14 +2,20 @@ package itcast.server;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import itcast.message.LoginRequestMessage;
+import itcast.message.LoginResponseMessage;
 import itcast.protocol.MessageCodecShareable;
 import itcast.protocol.ProtocolFrameDecoder;
+import itcast.server.service.UserService;
+import itcast.server.service.UserServiceFactory;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -34,6 +40,24 @@ public class ChatServer {
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
                     ch.pipeline().addLast(loggingHandler);
                     ch.pipeline().addLast(messageCodec);
+                    //  我只关心这个message的情况
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<LoginRequestMessage>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
+                            String username = msg.getUsername();
+                            String password = msg.getPassword();
+                            UserService userService = UserServiceFactory.getUserService();
+                            boolean login = userService.login(username, password);
+                            LoginResponseMessage responseMessage;
+                            if(login){
+                                responseMessage = new LoginResponseMessage(true, "登陆成功");
+                            }else {
+                                responseMessage = new LoginResponseMessage(false, "用户名或者密码错误");
+                            }
+                            // 然后再出站 解码 日志 半包粘包处理（倒着来）
+                            ctx.writeAndFlush(responseMessage);
+                        }
+                    });
                 }
             });
             ChannelFuture channelFuture = serverBootstrap.bind(8080);
