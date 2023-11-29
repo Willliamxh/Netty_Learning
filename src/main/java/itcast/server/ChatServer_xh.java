1ec3ae49838ea085ebf80dab68c85ba7f7046165
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-29 17:11:02 +0800

    聊天业务： 群聊-空闲检测

--- src/main/java/itcast/server/ChatServer_xh.java
+++ src/main/java/itcast/server/ChatServer_xh.java
@@ -1,13 +1,18 @@
 package itcast.server;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import itcast.protocol.MessageCodecShareable;
 import itcast.protocol.ProtocolFrameDecoder;
 import itcast.server.handler.*;
@@ -40,6 +45,22 @@ public class ChatServer_xh {
             serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
+                    // 用来判断是不是 读空闲时间过长，或 写空闲时间过长
+                    // 5s 内如果没有收到 channel 的数据，会触发一个 IdleState#READER_IDLE 事件
+                    ch.pipeline().addLast(new IdleStateHandler(5, 0, 0));
+                    // ChannelDuplexHandler 可以同时作为入站和出站处理器（messageCodec也继承了这个）
+                    ch.pipeline().addLast(new ChannelDuplexHandler() {
+                        // 用来触发特殊事件
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception{
+                            IdleStateEvent event = (IdleStateEvent) evt;
+                            // 触发了读空闲事件
+                            if (event.state() == IdleState.READER_IDLE) {
+                                log.debug("已经 5s 没有读到数据了");
+                                ctx.channel().close();
+                            }
+                        }
+                    });
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
                     ch.pipeline().addLast(loggingHandler);
                     ch.pipeline().addLast(messageCodec);
