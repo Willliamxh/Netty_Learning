7dbeaca1aaafff5231cec6f80122be069bcc4209
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-29 19:47:40 +0800

    netty优化： 扩展序列化算法-java序列化算法

--- src/main/java/itcast/protocol/MessageCodecShareable.java
+++ src/main/java/itcast/protocol/MessageCodecShareable.java
@@ -7,10 +7,6 @@ import io.netty.handler.codec.MessageToMessageCodec;
 import itcast.message.Message;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.List;
 
 /**
@@ -41,11 +37,13 @@ public class MessageCodecShareable extends MessageToMessageCodec<ByteBuf, Messag
         // 无意义，对齐填充 如果不加这个 就变成15个字节了 为了对象头对齐填充
         out.writeByte(0xff);
         // 6. 获取内容的字节数组
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        // 把对象转化成二进制数组
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-        oos.writeObject(msg);
-        byte[] bytes = bos.toByteArray();
+        // ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        // // 把对象转化成二进制数组
+        // ObjectOutputStream oos = new ObjectOutputStream(bos);
+        // oos.writeObject(msg);
+        // byte[] bytes = bos.toByteArray();
+        // 用重新写的Serializer来序列化
+        byte[] bytes = Serializer.Algorithm.Java.serialize(msg);
         // 7. 长度
         out.writeInt(bytes.length);
         // 8. 写入内容
@@ -64,8 +62,10 @@ public class MessageCodecShareable extends MessageToMessageCodec<ByteBuf, Messag
         int length = in.readInt();
         byte[] bytes = new byte[length];
         in.readBytes(bytes, 0, length);
-        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
-        Message message = (Message) ois.readObject();
+        // ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
+        // Message message = (Message) ois.readObject();
+        // 把java序列化方法抽象到一个类中
+        Message message = Serializer.Algorithm.Java.deserialize(Message.class, bytes);
         log.debug("{}, {}, {}, {}, {}, {}", magicNum, version, serializerType, messageType, sequenceId, length);
         log.debug("{}", message);
         out.add(message);
