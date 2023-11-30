5a0ef31b544e39b0cf2e708362b46a9962a441c4
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-30 11:16:42 +0800

    参数-backlog作用显示： accept queue - 全连接队列（全连接队列表示你一共有多少个客户端可以在服务器队列存放） * 其大小通过 /proc/sys/net/core/somaxconn 指定，在使用 listen 函数时，内核会根据传入的 backlog 参数与系统参数，取二者的较小值 * 如果 accpet queue 队列满了，server 将发送一个拒绝连接的错误信息到 client。 netty 中 可以通过  option(ChannelOption.SO_BACKLOG, 值) 来设置大小。 如果配置文件和程序都设置了，就会取两者的最小值。

---
+++ src/test/java/cn/itcast/netty_param_better/TestBacklogServer.java
@@ -0,0 +1,25 @@
+package cn.itcast.netty_param_better;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class TestBacklogServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                // 给ServerSocketChannel设置参数
+                .option(ChannelOption.SO_BACKLOG, 2) // 全队列满了，再练接就满了 如果来得及处理，就不会放在队列里
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new LoggingHandler());
+                    }
+                }).bind(8080);
+    }
+}
